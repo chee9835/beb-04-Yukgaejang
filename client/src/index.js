f12ce00c37fea213ac20d8c6b91675a68914c92f
nonon <nononcrust@gmail.com>
2022-06-13 15:15:52 +0900

    init

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { store } from "../store";
+import App from "./App";
+import GlobalStyle from "./styles/globalStyle";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
