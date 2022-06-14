ecbaa012c5305518c1f95680146e3a3ea2f2fa8b
yooseungjun <hongildong6987@gmail.com>
2022-06-14 19:01:25 +0900

    dd

--- client/src/pages/Explore.js
+++ client/src/pages/Explore.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import Card from "../components/common/Card";
 
 const DESCRIPTION_PLACEHOLDER =
-    'Autoglyphs are the first "on-chain generative art on the Ethereum blockchain. A completely self-con...';
+  'Autoglyphs are the first "on-chain generative art on the Ethereum blockchain. A completely self-con...';
 
 const Container = styled.section`
   .heading-wrapper {
@@ -62,34 +62,35 @@ const Container = styled.section`
     }
   }
 `;
-
+//여긴 그냥 크립토 펑크 가져오기
+//DB 구축
 const Explore = () => {
-    return (
-        <Container>
-            <div className="header" />
-            <div className="heading-wrapper">
-                <h1 className="heading">Explore Collections</h1>
-            </div>
-            <div className="tab-menu">
-                <div className="tab-wrapper">
-                    <span className="tab-text">Trending</span>
-                    <div className="tab-underline" />
-                </div>
-            </div>
-            <div className="contents">
-                <Card
-                    name="Autoglyphs"
-                    description={DESCRIPTION_PLACEHOLDER}
-                    author="NeoHuntersSolana"
-                />
-                <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
-                <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
-                <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
-                <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
-                <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
-            </div>
-        </Container>
-    );
+  return (
+    <Container>
+      <div className="header" />
+      <div className="heading-wrapper">
+        <h1 className="heading">Explore Collections</h1>
+      </div>
+      <div className="tab-menu">
+        <div className="tab-wrapper">
+          <span className="tab-text">Trending</span>
+          <div className="tab-underline" />
+        </div>
+      </div>
+      <div className="contents">
+        <Card
+          name="Autoglyphs"
+          description={DESCRIPTION_PLACEHOLDER}
+          author="NeoHuntersSolana"
+        />
+        <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
+        <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
+        <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
+        <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
+        <Card name="Autoglyphs" description={DESCRIPTION_PLACEHOLDER} />
+      </div>
+    </Container>
+  );
 };
 
 export default Explore;
--- smartcontract.sol
+++ smartcontract.sol
@@ -1,12 +1,11 @@
 // no license
 pragma solidity ^0.8.7;
 
-//RemixIDE에서 ABI,Bytecode만 뽑아오기
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
 
 contract YGJ is ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
@@ -25,6 +24,10 @@ contract YGJ is ERC721URIStorage, Ownable {
         _setTokenURI(newItemId, tokenURI);
 
         return newItemId;
+    }
+
+    function totalSupply() public view returns(uint256) {
+        return _tokenIds.current();
     }
 
     // function buy {
