2b58fc82dd705962846f6452192bd74876e8ada4
hongildong2 <48505911+hongildong2@users.noreply.github.com>
2022-06-17 09:53:27 +0900

    Update OwnerOnlyNFT.sol

--- smartcontract/OwnerOnlyNFT.sol
+++ smartcontract/OwnerOnlyNFT.sol
@@ -3,10 +3,11 @@ pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract cozNFTs is ERC721URIStorage, ERC721Enumerable {
+contract cozNFTs is ERC721URIStorage, Ownable, ERC721Enumerable {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
